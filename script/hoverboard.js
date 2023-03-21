f93fd34af265b61e6cfce45b0f802ae9cb590f8e
YikeChenn <chenyike1998@gmail.com>
2023-03-21 05:39:19 -0400

    Add loading animation, change on hover board part

--- script/hoverboard.js
+++ script/hoverboard.js
@@ -1,6 +1,6 @@
 const hoverBoard = document.getElementById('hoverBoard')
 const squareNum = 3956
-
+    
 for(let i = 0; i < squareNum; ++i){
     const square = document.createElement('div')
     square.classList.add('square')
@@ -8,24 +8,81 @@ for(let i = 0; i < squareNum; ++i){
     square.addEventListener('mouseout', () => removeColor(square))
     hoverBoard.appendChild(square)
 }
-
+    
 function randomColor(){
     var color = ""
     for(let i = 0; i < 6; ++i){
-        color += (Math.random() * 16 | 0).toString(16)
-    }
+    color += (Math.random() * 16 | 0).toString(16)
+}
     return "#" + color
 }
-
+    
 function setColor(ele){
     const color = randomColor()
     ele.style.background = color
     ele.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
-
+    
 function removeColor(ele){
     ele.style.background = '#1d1d1d'
     ele.style.boxShadow = '0 0 2px #000'
 }
+    
 
+
+$('#backgroundChange').click(function(){
+    if($('link[title=hoverboardCSS]').prop('disabled') == false){
+        $('#openSideButton').hover(function(){
+            let new_color = randomColor()
+            $(this).css({"background-color": new_color})
+        }, function(){
+            $(this).css({"background-color": "black"})
+        })
+            
+        $('.sideBar').hover(function(){
+            let new_color = randomColor()
+            $(this).css({
+                "background-color": new_color,
+                "border": "2px solid white"
+            })
+        }, function(){
+            $(this).css({
+                "background-color": "black",
+                "border": "none"
+            })
+        })
+    }
+    else{ 
+        $('.sideBar').hover(function(){
+            $(this).css({
+                "background-color": "rgb(0, 140, 255)",
+            })
+        }, function(){
+            $(this).css({
+                "background-color": "black",
+            })
+        })
+    }
+})
+
+
+// $('#openSideButton').hover(function(){
+//     let new_color = randomColor()
+//     $(this).css({"background-color": new_color})
+// }, function(){
+//     $(this).css({"background-color": "black"})
+// })
+    
+// $('.sideBar').hover(function(){
+//     let new_color = randomColor()
+//     $(this).css({
+//         "background-color": new_color,
+//         "border": "2px solid white"
+//     })
+// }, function(){
+//     $(this).css({
+//         "background-color": "black",
+//         "border": "none"
+//     })
+// })
 
--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
-$('link[title=hoverboardCSS]').prop('disabled',true);
-$('link[title=hoverboardJS]').prop('disabled',true);
+$('link[title=hoverboardCSS]').prop('disabled',true)
+
 
 //starting loading and bluring.
 $(document).ready(function(){
@@ -9,39 +9,35 @@ $(document).ready(function(){
         $('.loadingSign').fadeToggle(500)
     }, 2000)
 })
+let load = 0
+function blurring(){
+    load++
+    // if(load > 99) clearInterval()
+    document.querySelector('.page').style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
 
-
-// let load = 0
-// // let loading = setInterval(bluring, 30)
-// function blurring(){
-//     load++
-//     // if(load > 99) clearInterval()
-//     document.querySelector('.page').style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-// }
-// const scale = (num, in_min, in_max, out_min, out_max) => {
-//     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-// }
-
-
+//hide button
 $('#hideButton').click(function(){
     $('#hiddenPart').fadeToggle(500)
 })
 
 
-//side bar
+//side bar opener
 function ifOpenSidebar(){
     if(document.getElementById("sideBar").style.width == "20%") {closeSidebar()}
     else {openSidebar()}
 }
-
+//side bar
 function openSidebar(){
     $("#sideBar").animate({width: '20%'}, 500)
-    $("#sideMenu").animate({left: '20%'}, 500, console.log('finished'))
+    $("#sideMenu").animate({left: '20%'}, 500)
     $(".mainPart").animate({left: '26%'}, 500)
     $(".hiddenPage").animate({width: '70vw'}, 0)
 
 }
-
 function closeSidebar(){
     $("#sideBar").animate({width: '0'}, 500)
     $("#sideMenu").animate({left: '0'}, 500)
@@ -49,27 +45,48 @@ function closeSidebar(){
     $(".hiddenPage").animate({width: '75vw'}, 0)
 
 }
-
-
-// side bar 
 $(".sideBar").click(function(){
     $(".frontPage").removeClass("frontPage")
     $(".clicked").removeClass("clicked")
     $(this).addClass("clicked")
     if($(this).attr("id") == "introductionSideBar") $("#introPart").addClass("frontPage")
-    else if($(this).attr("id") == "vedioSideBar") $("#vedioPart").addClass("frontPage")
+    else if($(this).attr("id") == "articleSideBar") $("#articlePart").addClass("frontPage")
     else if($(this).attr("id") == "settingsSideBar") $("#settingPart").addClass("frontPage")
 })
 
 
 //Background Settings
 $(".starBackgroundSelection").click(function(){
-    $('link[title=starsCSS]').prop('disabled',false);
-    $('link[title=hoverboardCSS]').prop('disabled',true);
-    $('link[title=hoverboardJS]').prop('disabled',true);
+    new Promise(resolve => {
+        $('.loadingSign').fadeToggle(0)
+        resolve(1)
+    }).then(val =>{
+        new Promise(resolve =>{
+            $('link[title=starsCSS]').prop('disabled',false)
+            $('link[title=hoverboardCSS]').prop('disabled',true)
+        })
+    }).then(val =>{
+        new Promise(resolve =>{
+            $('.loadingSign').fadeToggle(500)
+        })
+    })
+
 })
 $(".hoverBoardBackgroundSelection").click(function(){
-    $('link[title=hoverboardCSS]').prop('disabled',false);
-    $('link[title=hoverboardJS]').prop('disabled',false);
-    $('link[title=starsCSS]').prop('disabled',true);
+    new Promise(resolve => {
+        $('.loadingSign').fadeToggle(0)
+        resolve(1)
+    }).then(val =>{
+        new Promise(resolve =>{
+            $('link[title=hoverboardCSS]').prop('disabled',false)
+            $('link[title=starsCSS]').prop('disabled',true)
+            resolve(2)
+        })
+    }).then(val =>{
+        new Promise(resolve =>{
+            setTimeout(function(){
+                $('.loadingSign').fadeToggle(500)
+            }, 3000)
+        })
+    })
 })
